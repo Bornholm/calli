81d109a17a9619da50d937ea766a1bbfa6917495
William Petit <wpetit@cadoles.com>
2025-08-12 22:24:00 +0200

    feat: extend filesystem testsuite

--- pkg/webdav/filesystem/testsuite/filesystem.go
+++ pkg/webdav/filesystem/testsuite/filesystem.go
@@ -2,12 +2,7 @@ package testsuite
 
 import (
 	"context"
-	"crypto/sha256"
-	"fmt"
-	"io"
 	"log/slog"
-	"os"
-	"path/filepath"
 	"testing"
 	"time"
 
@@ -25,61 +20,11 @@ type filesystemTestCase struct {
 var filesystemTestCases = []filesystemTestCase{
 	{
 		Name: "CreateRelativeDirectory",
-		Run: func(ctx context.Context, fs webdav.FileSystem) error {
-			if err := fs.Mkdir(ctx, "Test", os.ModePerm); err != nil && !errors.Is(err, os.ErrExist) {
-				return errors.WithStack(err)
-			}
-
-			path := "./Test/CreateRelativeDirectory"
-
-			if err := fs.Mkdir(ctx, path, os.ModePerm); err != nil {
-				return errors.WithStack(err)
-			}
-
-			fileInfo, err := fs.Stat(ctx, path)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			if e, g := filepath.Base(path), fileInfo.Name(); e != g {
-				return errors.Errorf("fileInfo.Name: expected '%s', got '%s'", e, g)
-			}
-
-			if !fileInfo.IsDir() {
-				return errors.Errorf("'%s' should be a directory", fileInfo.Name())
-			}
-
-			return nil
-		},
+		Run:  CreateRelativeDirectory,
 	},
 	{
 		Name: "CreateAbsoluteDirectory",
-		Run: func(ctx context.Context, fs webdav.FileSystem) error {
-			if err := fs.Mkdir(ctx, "Test", os.ModePerm); err != nil && !errors.Is(err, os.ErrExist) {
-				return errors.WithStack(err)
-			}
-
-			path := "Test/CreateAbsoluteDirectory"
-
-			if err := fs.Mkdir(ctx, path, os.ModePerm); err != nil {
-				return errors.WithStack(err)
-			}
-
-			fileInfo, err := fs.Stat(ctx, path)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			if e, g := filepath.Base(path), fileInfo.Name(); e != g {
-				return errors.Errorf("fileInfo.Name: expected '%s', got '%s'", e, g)
-			}
-
-			if !fileInfo.IsDir() {
-				return errors.Errorf("'%s' should be a directory", fileInfo.Name())
-			}
-
-			return nil
-		},
+		Run:  CreateAbsoluteDirectory,
 	},
 	{
 		Name: "WriteFile",
@@ -87,152 +32,28 @@ var filesystemTestCases = []filesystemTestCase{
 	},
 	{
 		Name: "ReadDir",
-		Run: func(ctx context.Context, fs webdav.FileSystem) error {
-			if err := fs.Mkdir(ctx, "Test", os.ModePerm); err != nil && !errors.Is(err, os.ErrExist) {
-				return errors.WithStack(err)
-			}
-
-			dir := "Test/ReadDir"
-
-			if err := fs.Mkdir(ctx, dir, os.ModePerm); err != nil {
-				return errors.WithStack(err)
-			}
-
-			directories := []string{
-				"sub1",
-				"sub2",
-			}
-
-			for _, n := range directories {
-				if err := fs.Mkdir(ctx, filepath.Join(dir, n), os.ModePerm); err != nil {
-					return errors.WithStack(err)
-				}
-			}
-
-			files := []string{
-				"1.txt",
-				"2.txt",
-				"sub1/3.txt",
-				"sub2/4.txt",
-				"sub2/5.txt",
-			}
-
-			for _, n := range files {
-				file, err := fs.OpenFile(ctx, filepath.Join(dir, n), os.O_CREATE, os.ModePerm)
-				if err != nil {
-					return errors.WithStack(err)
-				}
-
-				if err := file.Close(); err != nil {
-					return errors.WithStack(err)
-				}
-			}
-
-			dirFile, err := fs.OpenFile(ctx, dir, os.O_RDONLY, os.ModePerm)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			fileInfos, err := dirFile.Readdir(-1)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			if e, g := 4, len(fileInfos); e != g {
-				return errors.Errorf("len(fileInfos): expected '%d', got '%d'", e, g)
-			}
-
-			return nil
-		},
+		Run:  ReadDir,
 	},
 	{
 		Name: "LargeFileWrite",
-		Run: func(ctx context.Context, fs webdav.FileSystem) error {
-			tempDir, err := os.MkdirTemp("", "testdata-*")
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			defer os.RemoveAll(tempDir)
-
-			local, err := os.Create(filepath.Join(tempDir, "largefile"))
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			if err := local.Truncate(1e8); err != nil {
-				return errors.WithStack(err)
-			}
-
-			defer local.Close()
-
-			localStat, err := local.Stat()
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			localSHA, err := shasum(local)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			if _, err := local.Seek(0, io.SeekStart); err != nil {
-				return errors.WithStack(err)
-			}
-
-			remote, err := fs.OpenFile(ctx, "largefile", os.O_CREATE|os.O_RDWR, os.ModePerm)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			if _, err := io.Copy(remote, local); err != nil {
-				defer remote.Close()
-				return errors.WithStack(err)
-			}
-
-			if err := remote.Close(); err != nil {
-				return errors.WithStack(err)
-			}
-
-			remote, err = fs.OpenFile(ctx, "largefile", os.O_RDONLY, os.ModePerm)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			remoteStat, err := remote.Stat()
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			if e, g := localStat.Size(), remoteStat.Size(); e != g {
-				return errors.Errorf("remoteStat.Size(): expected '%d', got '%d'", e, g)
-			}
-
-			defer remote.Close()
-
-			remoteSHA, err := shasum(remote)
-			if err != nil {
-				return errors.WithStack(err)
-			}
-
-			if e, g := localSHA, remoteSHA; e != g {
-				return errors.Errorf("sha256: expected '%s', got '%s'", e, g)
-			}
-
-			return nil
-		},
+		Run:  LargeFileWrite,
 	},
-}
-
-func shasum(r io.Reader) (string, error) {
-	data, err := io.ReadAll(r)
-	if err != nil {
-		return "", errors.WithStack(err)
-	}
-
-	hash := sha256.Sum256(data)
-
-	return fmt.Sprintf("%x", hash), nil
+	{
+		Name: "DeleteFile",
+		Run:  DeleteFile,
+	},
+	{
+		Name: "ModifyFile",
+		Run:  ModifyFile,
+	},
+	{
+		Name: "FileMetadata",
+		Run:  FileMetadata,
+	},
+	{
+		Name: "RecursiveDirectory",
+		Run:  RecursiveDirectory,
+	},
 }
 
 func TestFileSystem(t *testing.T, fsType filesystem.Type, opts any) {
